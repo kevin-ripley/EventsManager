17fd9863abe21a7c24b8549ad796e8766c99f733
Kevin Ripley <kevin.r.ripley.kr@gmail.com>
2018-09-14 17:45:24 -0600

    another change to creds

--- src/app/credentials.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCAnoQNKd0EXKeAvMVjkVYb-IVwJCtYltQ",
-    authDomain: "eventsmanager-fall-2018.firebaseapp.com",
-    databaseURL: "https://eventsmanager-fall-2018.firebaseio.com",
-    projectId: "eventsmanager-fall-2018",
-    storageBucket: "",
-    messagingSenderId: "367993661039"
-
-};
